2b941e989c68dbdfdf52559fa66a1b4a3d723d25
Nick B <nbourrereg@hotmail.com>
2020-10-30 10:59:52 -0400

    Add project files.

---
+++ UserSecrets/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace UserSecrets
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IConfiguration Configuration;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            doConfig();
+        }
+
+        private void doConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", 
+                optional: true, 
+                reloadOnChange: true);
+
+            var devEnvVariable = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+
+            var isDevelopment = string.IsNullOrEmpty(devEnvVariable) || 
+                                    devEnvVariable.ToLower() == "development";
+
+            if (isDevelopment)
+            {
+                /// TODO : Do not forget to add the user secret file
+                /// 
+                builder.AddUserSecrets<MainWindow>();
+            }
+            
+            Configuration = builder.Build();
+
+            Debug.WriteLine($"A value in the appsettings.json : " +  $"{Configuration["defaultPath"]}");
+
+            var secondValue =
+                Configuration.GetSection("someConfigSection")["second"];
+
+            Debug.WriteLine($"Second value : {secondValue}");
+
+            var secretCode = Configuration["NuclearCode"];
+
+            Debug.WriteLine("The nuclear code is : " + 
+                secretCode);
+        }
+    }
+}
